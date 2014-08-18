0ab418cb4044593d0d8da0b945eed9735c7a9752
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-18 08:49:33 +0200

    + FIONBIO-FIONREAD-SIOCATMARK.c

---
+++ misc/FIONBIO-FIONREAD-SIOCATMARK.c
@@ -0,0 +1,17 @@
+/*
+ 
+    Program to determine the values for 2nd
+    parameter of the ioctl system call
+    on Windows.
+
+    gcc FIONBIO-FIONREAD-SIOCATMARK.c -o FIONBIO-FIONREAD-SIOCATMARK.exe
+
+*/
+#include <stdio.h>
+#include <winsock2.h>
+
+int main() {
+  printf("FIONBIO    : 0x%x   %12d\n", FIONBIO   , FIONBIO   );  // 0x8004667e    -2147195266
+  printf("FIONREAD   : 0x%x   %12d\n", FIONREAD  , FIONREAD  );  // 0x4004667f     1074030207
+  printf("SIOCATMARK : 0x%x   %12d\n", SIOCATMARK, SIOCATMARK);  // 0x40047307     1074033415
+}
