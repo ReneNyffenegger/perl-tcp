0fa5051325dd9a08e8013381c747ec0bbafe3c8b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2014-08-19 07:29:57 +0200

    + Functionality to read GET and POST parameters

--- tq84/HTTP/Request.pm
+++ tq84/HTTP/Request.pm
@@ -24,11 +24,62 @@ sub new { # {{{
     $self -> {http_verrsion}) = split ' ', $method_uri_version;
 
 
-    my $fields_;
    ($self -> {path},
-    $fields_)        = split '\?', $self->{uri}, 2;
+    $self -> {query_string}) = split '\?', $self->{uri}, 2;
+
+    $self -> determine_params;
+
 
     return $self;
+    
+} # }}}
+
+sub determine_params { # {{{
+    my $self = shift;
+
+    if (defined $self -> {query_string}) {
+      $self->determine_params_from_string($query_string);
+    }
+
+    if ($self->{method} eq 'POST') {
+      $self -> determine_params_from_string($self->{body});
+    }
+    
+} # }}}
+
+sub determine_params_from_string { # {{{
+    my $self   = shift;
+    my $string = shift;
+
+    my @name_values = split '&', $string;
+
+    for my $name_value (@name_values) {
+      my ($name, $value) = split '=', $name_value;
+
+      $name  = decode_($name );
+      $value = decode_($value);
+
+      $self -> {params} -> {$name} = $value;
+    }
+    
+} # }}}
+
+sub decode_ { # {{{
+
+    $t  = shift;
+    $t  =~ s/\+/ /g;
+    $t  =~ s/%(..)/chr(hex($1))/eg;
+
+    return $t;
+
+} # }}}
+
+sub param { # {{{
+
+    my $self = shift;
+    my $name = shift;
+
+    return $self->{params}->{$name};
     
 } # }}}
 
@@ -38,8 +89,6 @@ sub answer { # {{{
     my $status       = shift;
     my $content_type = shift;
     my $answer       = shift;
-
-    print $answer;
 
     $self->{socket_}->send( "HTTP/1.1 $status$nl");
     $self->{socket_}->send( "Content-Type: $content_type; charset=utf-8$nl");
